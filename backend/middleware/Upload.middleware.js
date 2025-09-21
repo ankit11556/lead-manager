dbb69482042d7b300961de272f989f22614e62d7
ankit <saharan442090@gmail.com>
2025-09-21 07:10:54 +0530

    create file name fuction in upload middleware

--- backend/middleware/Upload.middleware.js
+++ backend/middleware/Upload.middleware.js
@@ -1,11 +1,14 @@
 const multer = require('multer');
 const path = require('path');
 
-const storage =  multer.diskStorage({
-  destination: function(req, file, cb){
-    cb(null, Date.now()+path.extname(file.originalname))
-  }
-})
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "uploads/"); 
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
 
 function fileFilter(req, file, cb) {
   const filetypes = /csv|xlsx|xls/;
