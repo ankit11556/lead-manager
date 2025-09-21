f917dfdc2ebb90db7ef279ab50f27c73d9045228
ankit <saharan442090@gmail.com>
2025-09-21 07:08:22 +0530

    create upload middleware

---
+++ backend/middleware/Upload.middleware.js
@@ -0,0 +1,21 @@
+const multer = require('multer');
+const path = require('path');
+
+const storage =  multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null, Date.now()+path.extname(file.originalname))
+  }
+})
+
+function fileFilter(req, file, cb) {
+  const filetypes = /csv|xlsx|xls/;
+  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+  if (extname) {
+    cb(null, true);
+  }else{
+    cb(new Error("Only CSV, XLSX, and XLS files are allowed"))
+  }
+}
+
+const upload = multer({storage, fileFilter})
+module.exports = upload
