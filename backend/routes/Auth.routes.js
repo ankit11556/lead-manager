ba5b3bf7a65937d4f8f360427271c44661324c35
ankit <saharan442090@gmail.com>
2025-09-21 23:38:11 +0530

    create logout controlle, route & check auth route

--- backend/routes/Auth.routes.js
+++ backend/routes/Auth.routes.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
-const {registerController,loginController} = require("../controllers/User.controller")
+const {registerController,loginController,logout} = require("../controllers/User.controller")
+const {protectRoutes} = require("../middleware/Auth.middleware")
 
 router.post("/register",registerController)
 router.post("/login", loginController)
+router.post("/logout",logout)
+router.get("/check-auth",protectRoutes,(req,res)=>{
+  res.json({user: req.user})
+})
 
 module.exports = router      
